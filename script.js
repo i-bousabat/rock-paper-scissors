24abbf390cb0853e5963f36277ae226c85915e32
i-bousabat <imedbousabat1@gmail.com>
2025-05-18 17:39:43 -0400

    Add basic logic to obtain human and computer choice

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rand = Math.random();
     let choice;
 
     if (rand <= 0.33) {
-        choice = "Rock"
+        choice = "rock"
     
     }else if (rand <= 0.66){
-        choice = "Paper";
+        choice = "paper";
 
     }else {
-        choice = "Scissors";
+        choice = "scissors";
     }
-
+    return choice;
 }
 
+function getHumanChoice(){
+    choice = window.prompt("rock, paper or scissors");
+    return choice;
+}
 
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
